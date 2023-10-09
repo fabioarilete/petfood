fa5dbffeac612cb188f04553d902388de23396a3
fabioarilete <fabioarilete@hotmail.com>
2023-10-09 19:49:27 -0300

    feat:new features

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 
 import "./styles/global.css";
 import Cadastro from "./pages/cadastro";
+import Checkout from './pages/checkout';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Cadastro />
+    <Checkout />
   </React.StrictMode>
 );
---
+++ src/pages/checkout/index.js
@@ -0,0 +1,69 @@
+import Header from '../../components/header/index'
+
+import './styles.css'
+
+const Checkout = () =>{
+  return (
+    <div className="h-100" >
+      <Header />
+      <div className='container mt-4' >
+        <div className='row' >
+          <div className='col-6'>
+            <span className='section-title'>Dados de Entrega</span>
+            <div className='row mb-3'>
+              <div className='col-12'>
+                <input type="text" placeholder='CEP' className='form-control form-control-lg' />
+              </div>
+            </div>
+            <div className='row mb-3'>
+              <div className='col-6'>
+                <input type="text" placeholder='Cidade' className='form-control form-control-lg' />
+              </div>
+              <div className='col-6 pl-0'>
+                <input type="text" placeholder='Logradouro' className='form-control form-control-lg' />
+              </div>
+
+            </div>
+            <div className='row mb-3'>
+              <div className='col-5'>
+                <input type="text" placeholder='Número' className='form-control form-control-lg' />
+              </div>
+              <div className='col-5 pl-0'>
+                <input type="text" placeholder='Bairro' className='form-control form-control-lg' />
+              </div>
+              <div className='col-2 pl-0'>
+                <input type="text" placeholder='UF' className='form-control form-control-lg' />
+              </div>
+            </div>
+            <span className='section-title'>Dados de Pagamento</span>
+            <div className='row mb-3'>
+              <div className='col-12'>
+                <input type="text" placeholder='Número do cartão' className='form-control form-control-lg' />
+              </div>
+            </div>
+            <div className='row mb-3'>
+              <div className='col-6 pl-0'>
+                <input type="text" placeholder='Validade' className='form-control form-control-lg' />
+              </div>
+              <div className='col-6 pl-0'>
+                <input type="text" placeholder='CVV' className='form-control form-control-lg' />
+              </div>
+
+            </div>
+            <div className='row mb-3'>
+              <div className='col-7'>
+                <input type="text" placeholder='Nome do titular' className='form-control form-control-lg' />
+              </div>
+              <div className='col-5 pl-0'>
+                <input type="text" placeholder='CPF / CNPJ do titular' className='form-control form-control-lg' />
+              </div>
+  
+            </div>
+          </div>
+          <div className='col-6'></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Checkout
